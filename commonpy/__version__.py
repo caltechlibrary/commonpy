f43043ee92866e5e09337d28509489840f6194d5
Michael Hucka <mhucka@caltech.edu>
2018-04-18 15:16:05 -0700

    Add standard __version__.py file

---
+++ commonpy/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   commonpy version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/commonpy
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'commonpy'
+__description__ = 'Common Python Modules'
+__url__         = 'https://github.com/caltechlibrary/commonpy'
+__author__      = 'Caltech Digital Library Development group'
+__email__       = 'dld@library.caltech.edu'
+__license__     = 'BSD/MIT derivative -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
