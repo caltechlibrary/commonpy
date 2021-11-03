02f65e06e715c54d6a94e3505c69bba88aa6a70c
mhucka <mhucka@caltech.edu>
2021-11-02 18:06:22 -0700

    Update stored version number

--- commonpy/__init__.py
+++ commonpy/__init__.py
@@ -21,7 +21,7 @@ file "LICENSE" for more information.
 #  |    by the Makefile. Manual changes to these values will be lost.      |
 #  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
 
-__version__     = '1.5.0'
+__version__     = '1.6.0'
 __description__ = 'Assortment of Python helper functions and utility classes'
 __url__         = 'https://github.com/caltechlibrary/commonpy'
 __author__      = 'Michael Hucka'
