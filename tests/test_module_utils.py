fc0d897e2bffcdfc9ce4a2ae362965ccd70238b7
mhucka <mhucka@caltech.edu>
2022-03-14 19:28:01 -0700

    Add some more simple tests of module utils

--- tests/test_module_utils.py
+++ tests/test_module_utils.py
@@ -12,6 +12,16 @@ except:
 from commonpy.module_utils import *
 
 
-def test_writable(tmpdir):
+def test_configpath():
     if not sys.platform.startswith('win'):
         assert config_path('foo') == path.join(path.expanduser('~'), '.config', 'foo')
+
+
+def test_desktop_path():
+    expected = path.join(path.expanduser('~'), 'Desktop')
+    if not sys.platform.startswith('win'):
+        assert desktop_path() == expected
+
+
+def test_datadir_path():
+    assert datadir_path().endswith('data')
