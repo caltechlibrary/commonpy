9107d847372fb1fa88872fecdfe98b64482f7089
mhucka <mhucka@caltech.edu>
2022-03-14 19:52:29 -0700

    Add tests for print_boxed()

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -1,3 +1,5 @@
+from   contextlib import redirect_stdout
+import io
 import os
 import pytest
 import sys
@@ -20,3 +22,38 @@ def test_antiformat(tmpdir):
     assert antiformat('foo {bar}').format(bar = 'biff') == 'foo {bar}'
     assert antiformat(Exception('test exception')) == 'test exception'
     assert antiformat(b'foo') == "b'foo'"
+
+
+def test_print_boxed1():
+    output = io.StringIO()
+    with redirect_stdout(output):
+        print_boxed('foo')
+    assert output.getvalue() == ",-----.\n| foo |\n`-----'\n"
+
+
+def test_print_boxed2():
+    output = io.StringIO()
+    with redirect_stdout(output):
+        print_boxed('foo\nbar')
+    assert output.getvalue() == ",-----.\n| foo |\n| bar |\n`-----'\n"
+
+
+def test_print_boxed3():
+    output = io.StringIO()
+    with redirect_stdout(output):
+        print_boxed('foo\nbar', indent = 2)
+    assert output.getvalue() == ",-------.\n|  foo  |\n|  bar  |\n`-------'\n"
+
+
+def test_print_boxed4():
+    output = io.StringIO()
+    with redirect_stdout(output):
+        print_boxed('foo\nbar', width = 8)
+    assert output.getvalue() == ",----------.\n| foo      |\n| bar      |\n`----------'\n"
+
+
+def test_print_boxed5():
+    output = io.StringIO()
+    with redirect_stdout(output):
+        print_boxed('foo\nbar', title = 'wow')
+    assert output.getvalue() == ",-----.\n| wow |\n| --- |\n| foo |\n| bar |\n`-----'\n"
