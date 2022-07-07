0a1ca39a5b09dc3283607c73e16d3976bd9e17f4
mhucka <mhucka@caltech.edu>
2022-07-07 14:19:48 -0700

    Improve parsing of requirements.txt
    This now handles requirements.txt syntax a bit better:
    - comment lines are stripped
    - extended syntax is detected and pip's parser is used if possible

--- setup.py
+++ setup.py
@@ -6,23 +6,32 @@
 # @license Please see the file named LICENSE in the project directory
 # @website https://github.com/caltechlibrary/commonpy
 #
-# Note: configuration metadata is maintained in setup.cfg.  This file exists
-# primarily to hook in setup.cfg and requirements.txt.
-#
+# Note: the full configuration metadata is maintained in setup.cfg, not here.
+# This file exists to hook in setup.cfg and requirements.txt, so that the
+# requirements don't have to be repeated and so that "python3 setup.py" works.
 # =============================================================================
 
-import os
-from   os.path import exists, join, abspath, dirname
-from   setuptools import setup
+from os.path import abspath, dirname, exists, join
+from setuptools import setup
 
-here = abspath(dirname(__file__))
-
-requirements = []
-if exists(join(here, 'requirements.txt')):
-    with open(join(here, 'requirements.txt')) as f:
-        requirements = f.read().rstrip().splitlines()
+required = []
+requirements_file = join(abspath(dirname(__file__)), 'requirements.txt')
+if exists(requirements_file):
+    with open(requirements_file, encoding = 'utf-8') as f:
+        required = [line for line in filter(str.strip, f.read().splitlines())
+                    if not line.startswith('#')]
+    if any(item.startswith(('-', '.', '/')) for item in required):
+        # The requirements.txt uses pip features. Try to use pip's parser.
+        try:
+            from pip._internal.req import parse_requirements
+            from pip._internal.network.session import PipSession
+            parsed = parse_requirements(requirements_file, PipSession())
+            required = [item.requirement for item in parsed]
+        except ImportError:
+            # No pip, or not the expected version. Give up & leave list as is.
+            pass
 
 setup(
     setup_requires = ['wheel'],
-    install_requires = requirements,
+    install_requires = required,
 )
